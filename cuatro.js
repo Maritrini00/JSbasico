229decdb21b4bc4c3013ba0dfd441322f4dddecc
Maritrini Velázquez <a329675@uach.mx>
2021-04-26 00:03:01 -0600

    programa que imrpime la suma de los primeros 100 numeros

---
+++ cuatro.js
@@ -0,0 +1,9 @@
+function sumaTodos(){
+    let suma = 0;
+    for(let i=0;i<=100;i++){
+        suma +=i;
+    }
+    return suma;
+}
+
+console.log("la suma de los numeros del 0 al 100 es: "+sumaTodos());
