0f510271dee534f50ee98ee153b6be3f95fa3ee2
Maritrini Velázquez <a329675@uach.mx>
2021-04-27 16:20:18 -0600

    Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta era el 32 pero lo confundi

---
+++ treintaycinco.js
@@ -0,0 +1,8 @@
+let nombres  = ['Hugo','Paco','Luis','Mickey','Minnie','Daisy','Donald','Goofy','Pluto','Max',
+                'Maria','Fernando','Betty','Armando','Paty','Daniel','Daniela','Toño','Leo','Esmeralda'];
+
+let tam =nombres.length; 
+
+for(let i=0;i<tam;i++){
+    console.log(nombres[i]);
+}
