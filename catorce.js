412311432bca5c4129d585417279a2298b8f06c8
Maritrini Velázquez <a329675@uach.mx>
2021-04-27 14:07:47 -0600

    maximo y minimo de 5 numeros

---
+++ catorce.js
@@ -0,0 +1,39 @@
+/* const prompt = require('prompt-sync')({sigint: true});
+ 
+// Random number from 1 - 10
+const numberToGuess = Math.floor(Math.random() * 10) + 1;
+// This variable is used to determine if the app should continue prompting the user for input
+let foundCorrectNumber = false;
+ 
+while (!foundCorrectNumber) {
+  // Get user input
+  let guess = prompt('Guess a number from 1 to 10: ');
+  // Convert the string input to a number
+  guess = Number(guess);
+ 
+  // Compare the guess to the secret answer and let the user know.
+  if (guess === numberToGuess) {
+    console.log('Congrats, you got it!');
+    foundCorrectNumber = true;
+  } else {
+    console.log('Sorry, guess again!');
+  }
+} */
+
+const prompt = require('prompt-sync')({sigint: true});
+let max= 0;
+let min =9999
+let cont=0;
+while(cont<5){
+  let num = prompt('escribe un numero: ');
+  num = Number(num);
+  if(num>max){
+    max=num;
+  }
+  if(num<min){
+    min=num;
+  }
+  cont++;
+}
+console.log(`numero maximo: ${max}`);
+console.log(`numero minimo: ${min}`);
